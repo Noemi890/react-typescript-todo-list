141d09b0b862d4d0a3536310cdc3376462ae1ab2
Noemi <noemi.caggiano@gmail.com>
2023-03-17 10:59:26 +0000

    feat: cleanup and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { FC } from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
-      
+      <div className='header'>
+        <div className='input_container'>
+        <input type="text" placeholder='Task..'/>
+        <input type="number" placeholder='Deadline (in Days)..'/>
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className='todoList'></div>
     </div>
   );
 }
